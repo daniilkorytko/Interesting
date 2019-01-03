32dcd0b491967d94ba3fade8f30a0d158bfd6ff4
daniilkorytko <43876618+daniilkorytko@users.noreply.github.com>
2019-01-03 14:21:01 +0300

    Add files via upload

---
+++ CreataAzureApp.ps1
@@ -0,0 +1,32 @@
+﻿$ResourceGroup = "DaniilK"
+$LOcation = "westeurope"
+
+New-AzureRmResourceGroup -Name $ResourceGroup -Location $LOcation -Force
+
+$textFunction = "#r `"Newtonsoft.Json`"
+
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+
+public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
+{
+    log.LogInformation(`"C# HTTP trigger function processed a request.`");
+
+    string name = req.Query[`"name`"];
+
+    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+    dynamic data = JsonConvert.DeserializeObject(requestBody);
+    name = name ?? data?.name;
+
+    return name != null
+        ? (ActionResult)new OkObjectResult($`"Hello, {name}`")
+        : new BadRequestObjectResult(`"Please pass a name on the query string or in the request body`");
+}
+"
+
+
+$deployment = New-AzureRmResourceGroupDeployment  -ResourceGroupName $ResourceGroup `
+   -TemplateFile "C:\Work\Azure Part 2\AzureApp\DaniilAzureApp.json" -Verbose -AzureFunction "$textFunction"
+
